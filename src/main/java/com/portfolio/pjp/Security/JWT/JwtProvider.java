3577e75489fa4c5435aeed3bbb1ed6f55489e805
Pejotapp <106160171+Pejotapp@users.noreply.github.com>
2022-10-27 15:09:23 -0300

    cambios Persona Interface, ImpPersonaS, PersonaControler

--- src/main/java/com/portfolio/pjp/Security/JWT/JwtProvider.java
+++ src/main/java/com/portfolio/pjp/Security/JWT/JwtProvider.java
@@ -32,7 +32,8 @@ public class JwtProvider {
                 .setIssuedAt(new Date())
                 .setExpiration(new Date(new Date().getTime()+ expiration *1000))
                 .signWith(SignatureAlgorithm.HS512, secret)
-                .compact();}
+                .compact();
+    }
     
 public String getNombreUsuarioFromToken(String token){
     return Jwts.parser().setSigningKey(secret).parseClaimsJws(token).getBody().getSubject();}
@@ -40,14 +41,15 @@ public String getNombreUsuarioFromToken(String token){
 public boolean validateToken(String token){
 try{
     Jwts.parser().setSigningKey(secret).parseClaimsJws(token);
-
-return true;}
-catch (MalformedJwtException e){logger.error("Token mal formado");}
-catch (UnsupportedJwtException e){logger.error("Token no soportado");}
-catch (ExpiredJwtException e){logger.error("Token expirado");}
-catch (IllegalArgumentException e){logger.error("Token vacio");}
-catch (SignatureException e){logger.error("Firma no válida");}
-return false;}}
+return true;
+} catch (MalformedJwtException e){logger.error("Token mal formado");
+} catch (UnsupportedJwtException e){logger.error("Token no soportado");
+} catch (ExpiredJwtException e){logger.error("Token expirado");
+} catch (IllegalArgumentException e){logger.error("Token vacio");
+} catch (SignatureException e){logger.error("Firma no válida");}
+return false;
+}
+}
 
     
 
--- src/main/java/com/portfolio/pjp/Service/ImpPersonaService.java
+++ src/main/java/com/portfolio/pjp/Service/ImpPersonaService.java
@@ -6,43 +6,59 @@ package com.portfolio.pjp.Service;
 
 import com.portfolio.pjp.Entity.Persona;
 import com.portfolio.pjp.Repository.IPersonaRepository;
-import java.util.List;
-import java.util.Optional;
 import javax.transaction.Transactional;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 @Transactional
-public class ImpPersonaService{
+public class ImpPersonaService implements IPersonaService{
 
     @Autowired
     IPersonaRepository ipersonaRepository;
 
-    
-     public List<Persona> list() {
-    return ipersonaRepository.findAll();
+     @Override
+    public List<Persona> getPersona() {
+        List<Persona> persona = ipersonaRepository.findAll();
+        return persona;
     }
     
-    public Optional<Persona> getOne(int id){
-    
-    return ipersonaRepository.findById(id);
-    }
-    
-    public Optional<Persona> getByNombre(String nombre){
-    return ipersonaRepository.findByNombre(nombre);}
-    
-    public void save(Persona persona){
-     ipersonaRepository.save(persona);}
-    
-    public void delete(int id){
+    @Override
+    public void deletePersona(int id){
     ipersonaRepository.deleteById(id);}
     
-    public boolean existsById(int id){
-    return ipersonaRepository.existsById(id);}
+ 
+    @Override
+    public void savePersona(Persona persona){
+    ipersonaRepository.save(persona);
+    }
     
-    public boolean existsByNombre(String nombre){
-    return ipersonaRepository.existsByNombre(nombre);}
+     @Override
+    public Persona traerPersona(int id) {
+        return ipersonaRepository.getById(id);
+    }
     
-    
+ 
 }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
